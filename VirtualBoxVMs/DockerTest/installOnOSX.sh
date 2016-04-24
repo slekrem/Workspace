37ecae6dfeca66e625430768503dc152f7efa5c2
Stefan Merkel <stefanmerkel@Stefans-MacBook-Pro.local>
2016-04-24 22:27:04 +0200

    Update

---
+++ VirtualBoxVMs/DockerTest/installOnOSX.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+if [[ ! -d "./bin" ]]; then
+  mkdir bin
+fi
---
+++ initWorkspaceOnOSX.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# install homebrew
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# instal apps
+brew install cask
+
+# install casks
+brew cask install virtualbox
+brew cask install google-chrome
+brew cask install sourcetree
+brew cask install visual-studio-code
+brew cask install iterm2
+brew cask install atom
+
+# install terminal-plus
+apm install terminal-plus
