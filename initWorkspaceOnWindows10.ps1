0f829d224ba0fe3b314f3c576a61b15def6e4640
pandawan91 <vuongngo@outlook.de>
2016-04-25 22:14:33 +0200

    Update initWorkspaceOnWindows10.ps1

--- initWorkspaceOnWindows10.ps1
+++ initWorkspaceOnWindows10.ps1
@@ -1,15 +1,17 @@
-#Installiere Chocolaty Paket-Manager
+#install Chocolaty Paket-Manager
 (iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')))>$null 2>&1
 
-#Installiere Tools 
+#install Tools 
 choco install google-chrome-x64 -y
 
 choco install notepadplusplus.install -y
+choco install atom -y
 choco install visualstudio2015enterprise -y
 choco install intellijidea-community -y
 choco install mysql.workbench -y
 choco install aspnet5 -y
 choco install sourcetree -y
+choco install nodejs.install -y
 
 choco install unity -y
 choco install unitywebplayer -y 
@@ -22,3 +24,14 @@ choco install virtualbox -y
 choco install steam -y
 
 choco install 7zip.install -y
+
+#install atom plugins
+apm install omnisharp-atom
+apm install pigments
+apm install minimap
+apm install autocomplete-plus
+apm install language-cshtml
+apm install linter
+apm install linter-docker
+apm install language-docker
+apm install language-yaml
