d3765f6f5c1ec33075241c7bb03e5af83c821b4e
pandawan91 <vuongngo@outlook.de>
2016-04-25 21:14:11 +0200

    Update initWorkspaceOnWindows10.ps1

--- initWorkspaceOnWindows10.ps1
+++ initWorkspaceOnWindows10.ps1
@@ -1,12 +1,5 @@
-#Powershell erlauben scripts auszuführen
-Set-ExecutionPolicy Unrestricted 
-
 #Installiere Chocolaty Paket-Manager
 (iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')))>$null 2>&1
 
 #Installiere Tools 
 choco install google-chrome-x64
-
-
-#Powershell wieder verbieten
-Set-ExecutionPolicy Restricted 
