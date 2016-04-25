c784d37adac94389e805fc4b8a52f1cc7c7211a1
Stefan Merkel <merkel.s.ulm@gmail.com>
2016-04-26 00:46:28 +0200

    update

--- VirtualBox/createVm.sh
+++ VirtualBox/createVm.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-clear
+#clear
 
 # variables
-$vmName = "VirtualMachine"
-$vmMemory = 512
+vmName="VirtualMachine"
+vmMemory=512
+vmAacaddress1=BCAF4EE72171
+vmOstype=Ubuntu
 
 # create vm
 VBoxManage createvm --name $vmName --register
 
 # modify vm
 VBoxManage modifyvm $vmName --memory $vmMemory --acpi on --boot1 dvd
-#VBoxManage modifyvm "io" --nic1 bridged --bridgeadapter1 eth0
-#VBoxManage modifyvm "io" --macaddress1 XXXXXXXXXXXX
-#VBoxManage modifyvm "io" --ostype Debian
+VBoxManage modifyvm $vmName --nic1 bridged --bridgeadapter1 eth0
+VBoxManage modifyvm $vmName --macaddress1 $vmMacAddress1
+VBoxManage modifyvm "io" --ostype Debian
+
+
+
+echo asd
+VBoxManage unregistervm $vmName --delete
--- initWorkspaceOnOSX.sh
+++ initWorkspaceOnOSX.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # install homebrew
-#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # instal apps
 brew install cask
 
 # install casks
 brew cask install virtualbox
+brew cask install boot2docker
 brew cask install google-chrome
 brew cask install sourcetree
 brew cask install visual-studio-code
@@ -15,5 +16,6 @@ brew cask install iterm2
 brew cask install atom
 
 # install terminal-plus
+apm install npm-install
 apm install terminal-plus
 apm install script
