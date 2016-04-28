05981edccce9fc3521ea2b72f2324214d5bf0f91
Stefan Merkel <merkel.s.ulm@gmail.com>
2016-04-28 17:56:25 +0200

    update

--- initWorkspaceOnOSX.sh
+++ initWorkspaceOnOSX.sh
@@ -5,15 +5,20 @@
 
 # instal apps
 brew install cask
+brew install openssl
+
+brew link --force openssl
 
 # install casks
 brew cask install virtualbox
 brew cask install docker-machine
+brew cask install docker-compose
 brew cask install google-chrome
 brew cask install sourcetree
 brew cask install visual-studio-code
 brew cask install iterm2
-brew cask install atom
+brew cask install atom # kann auch gebaut werden ...
+brew cask install node
 
 # install terminal-plus
 apm install npm-install
